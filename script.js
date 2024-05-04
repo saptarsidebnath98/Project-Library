e497f689bc1ceb0bc58c106be8acafed9618e6b2
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-05-04 14:48:41 +0530

    constructor replaced with class

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-  // the constructor
-  (this.title = title),
-    (this.author = author),
-    (this.pages = pages),
-    (this.read = read);
+class Book{
+  // the class
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+
 }
 function removeBook(index){
     myLibrary.splice(index, 1);
